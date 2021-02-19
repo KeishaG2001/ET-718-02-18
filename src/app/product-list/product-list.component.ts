1c5d1bfdfcd231c0a61ec1806f27d007d6454925
KeishaG2001 <kgangarum@prep4writers.com>
2021-02-19 17:42:44 -0500

    second phase

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,12 +9,16 @@ import { products } from '../products';
 })
 export class ProductListComponent {
   products = products;
-
+ 
   share() {
     window.alert('The product has been shared!');
   }
-}
 
+  
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
 
 /*
 Copyright Google LLC. All Rights Reserved.
